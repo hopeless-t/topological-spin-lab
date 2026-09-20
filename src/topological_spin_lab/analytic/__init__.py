1c0b25e6bd14f688733802b6f683aa6b427bc4d2
Catfood Lab <274503048+hopeless-t@users.noreply.github.com>
2026-09-20 20:11:45 +0900

    exp002: export magnetic analytic reference

--- src/topological_spin_lab/analytic/__init__.py
+++ src/topological_spin_lab/analytic/__init__.py
@@ -1,3 +1,12 @@
+from .magnetic_surface_dirac import (
+    analytic_magnetic_energies,
+    analytic_magnetic_spin,
+)
 from .surface_dirac import analytic_energies, analytic_spin
 
-__all__ = ["analytic_energies", "analytic_spin"]
+__all__ = [
+    "analytic_energies",
+    "analytic_spin",
+    "analytic_magnetic_energies",
+    "analytic_magnetic_spin",
+]
