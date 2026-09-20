4f9f97875be170408cfe6d66c21d55e127277b0d
Catfood Lab <274503048+hopeless-t@users.noreply.github.com>
2026-09-20 20:33:57 +0900

    exp003: export domain-wall experiment

--- src/topological_spin_lab/experiments/__init__.py
+++ src/topological_spin_lab/experiments/__init__.py
@@ -1,4 +1,5 @@
 from .exp001 import execute_exp001
 from .exp002 import execute_exp002
+from .exp003 import execute_exp003
 
-__all__ = ["execute_exp001", "execute_exp002"]
+__all__ = ["execute_exp001", "execute_exp002", "execute_exp003"]
